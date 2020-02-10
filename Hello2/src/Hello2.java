1e502968fad9b20476f0545cb21354a098a9aebe
ChaeyeonYu <uoah8230@naver.com>
2020-02-10 15:02:47 +0900

    append hi2(master)

--- Hello2/src/Hello2.java
+++ Hello2/src/Hello2.java
@@ -3,5 +3,6 @@ public class Hello2 {
 	public static void main(String[] args) {
 		System.out.println("Hello2");
 		System.out.println("Hello2(hotfix)");
+		System.out.println("Hi2(master)");
 	}
 }
